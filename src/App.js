5b9441ae1b690d6e23a5cb10eee053d2474d9ab3
shuwh <shuwh317@gmail.com>
2019-02-20 12:44:15 -0800

    basic test of functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Contact from './components/Contact'
+import Header from './components/Header'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header branding='Contact Manager' />
+        <Contact name='John Doe' email='jdoe@gmail.com' phone='111 111-1111' />
+        <Contact name='Karen Smith' email='karen@gmail.com' phone='222 222-2222' />
       </div>
     );
   }
