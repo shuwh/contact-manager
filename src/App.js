c0d19fe602e283fb6ee00e911cac5f970bbbc54f
Winston <shuwh317@gmail.com>
2019-02-21 23:07:52 -0800

    add jquery and popper

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import Contacts from './components/Contacts';
 import Header from './components/Header';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
