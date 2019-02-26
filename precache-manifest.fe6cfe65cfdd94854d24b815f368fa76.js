50df49d16608817cdbd081120af7b4b8c5cb6472
Winston <shuwh317@gmail.com>
2019-02-25 21:46:57 -0800

    Updates

---
+++ precache-manifest.fe6cfe65cfdd94854d24b815f368fa76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d93b44bdc16848bb79d",
+    "url": "/contact-manager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "4d93b44bdc16848bb79d",
+    "url": "/contact-manager/static/js/main.4d93b44b.chunk.js"
+  },
+  {
+    "revision": "85e3bf223889503391a1",
+    "url": "/contact-manager/static/js/runtime~main.85e3bf22.js"
+  },
+  {
+    "revision": "7b1547d97d0f7855975c",
+    "url": "/contact-manager/static/css/2.f93969d6.chunk.css"
+  },
+  {
+    "revision": "7b1547d97d0f7855975c",
+    "url": "/contact-manager/static/js/2.7b1547d9.chunk.js"
+  },
+  {
+    "revision": "b6890ea146b0277c30bfb8c1e14ea0a1",
+    "url": "/contact-manager/index.html"
+  }
+];
